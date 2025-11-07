b05bb7fdc3ba6cdaf8bc8efa1272645864193e0f
Matthias Pall Gissurarson <mpg@mpg.is>
2025-11-07 10:31:07 +0100

    Some more helpers

--- app/Test.hs
+++ app/Test.hs
@@ -2,24 +2,34 @@
 module Main where
 import Lib
 import GHC.Wasm.Prim
-
+import Data.String (IsString(..))
 
 -- -- Export the actual initialization function
 foreign export javascript "main" main :: IO ()
+
+
+rotateSprite :: JSVal -> JSVal -> IO JSVal
+rotateSprite sprite time = do
+    dt <- valAsFloat <$> getProperty "deltaTime" time
+    incrementProperty "rotation" sprite (floatAsVal 0.01)
+    return sprite
 
 main :: IO ()
 main = do
     app <- newApp
     app <- initApp app $ toJSString "black"
     appendCanvas app
-    screen_width <- getScreenWidth app
-    screen_height <- getScreenHeight app
+    screen <- getProperty "screen" app
+    screen_width <- valAsInt <$> getProperty "width" screen
+    screen_height <- valAsInt <$> getProperty "height" screen
     texture <- loadAsset $ toJSString "assets/bunny.png"
     sprite <- newSprite texture
     consoleLogShow screen_width
     consoleLogShow screen_height
     setAnchor sprite 0.5
-    setX sprite (fromIntegral screen_width / 2.0)
-    setY sprite (fromIntegral screen_height / 2.0)
+    setProperty "x" sprite (floatAsVal (fromIntegral screen_width / 2.0))
+    setProperty "y" sprite (floatAsVal (fromIntegral screen_height / 2.0))
     addChild app sprite
+    func <- jsFuncFromHs (rotateSprite sprite)
+    addTicker app func
     return ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,8 +3,7 @@ module Lib where
 
 import GHC.Wasm.Prim
 
-succ :: Int -> Int
-succ x = x + 1
+import Data.String (IsString(..))
 
 type JSFunction = JSVal
 
@@ -18,19 +17,15 @@ foreign import javascript safe
  """
  initApp :: JSVal -> JSString -> IO JSVal
 
+
 foreign import javascript unsafe "document.body.appendChild($1.canvas)"
     appendCanvas :: JSVal -> IO ()
 
 foreign import javascript unsafe "console.log($1)"
-    consoleLog :: JSVal -> IO ()
-foreign import javascript unsafe "console.log($1)"
     consoleLogVal :: JSVal -> IO ()
 
-foreign import javascript unsafe "console.log($1)"
-    consoleLogString :: JSString -> IO ()
-
 consoleLogShow :: Show a => a -> IO ()
-consoleLogShow = consoleLogString . toJSString . show
+consoleLogShow = consoleLogVal . stringAsVal . toJSString . show
 
 foreign import javascript safe "const texture = await Assets.load($1); return texture"
     loadAsset :: JSString -> IO JSVal
@@ -41,20 +36,50 @@ foreign import javascript unsafe "new Sprite($1)"
 foreign import javascript unsafe "$1.anchor.set($2)"
     setAnchor :: JSVal -> Float -> IO ()
 
-foreign import javascript unsafe "$1.x = $2"
-    setX :: JSVal -> Float -> IO ()
-
-foreign import javascript unsafe "$1.y = $2"
-    setY :: JSVal -> Float -> IO ()
-
-foreign import javascript unsafe "$1.screen.width"
-    getScreenWidth :: JSVal -> IO Int
-
-foreign import javascript unsafe "$1.screen.height"
-    getScreenHeight :: JSVal -> IO Int
-
 foreign import javascript unsafe "$1.stage.addChild($2)"
     addChild :: JSVal -> JSVal -> IO ()
 
+foreign import javascript unsafe "$1($2)"
+  callFunction :: JSFunction -> JSVal -> IO JSVal
+
 foreign import javascript unsafe "$1.ticker.add($2)"
     addTicker :: JSVal -> JSFunction -> IO ()
+
+foreign import javascript "wrapper"
+  jsFuncFromHs :: (JSVal -> IO JSVal) -> IO JSVal
+
+foreign import javascript "$2[$1]"
+  getProperty :: JSString -> JSVal -> IO JSVal
+
+foreign import javascript "$2[$1] = $3"
+  setProperty ::  JSString -> JSVal -> JSVal -> IO ()
+
+foreign import javascript "$1"
+  valAsFloat :: JSVal -> Float
+
+foreign import javascript "$1"
+  valAsInt :: JSVal -> Int
+
+foreign import javascript "$1"
+  valAsBool :: JSVal -> Bool
+
+foreign import javascript "$1"
+  valAsString :: JSVal -> JSString
+
+foreign import javascript "$1"
+  floatAsVal :: Float -> JSVal
+
+foreign import javascript "$1"
+  intAsVal :: Int -> JSVal
+
+foreign import javascript "$1"
+  boolAsVal :: Bool -> JSVal
+
+foreign import javascript "$1"
+  stringAsVal :: JSString -> JSVal
+
+foreign import javascript "$2[$1] += $3"
+  incrementProperty :: JSString -> JSVal -> JSVal -> IO ()
+
+instance IsString JSString where
+    fromString = toJSString
